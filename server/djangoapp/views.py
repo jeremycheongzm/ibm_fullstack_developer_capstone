7c42b43e38841ec06e09b2245ce2a73bef498046
jeremycheongzm <zmcheong.2021@scis.smu.edu.sg>
2025-12-09 10:49:56 -0500

    dynamic pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -90,6 +90,7 @@ def get_dealerships(request):
     return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
+'''
 def get_dealer_reviews(request,dealer_id):
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
@@ -97,6 +98,22 @@ def get_dealer_reviews(request,dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status": 200, "reviews": reviews})
+    else:
+        return JsonResponse({"status": 400, "message": "Bad Request"})
+'''
+def get_dealer_reviews(request, dealer_id):
+    if dealer_id:
+        endpoint = f"/fetchReviews/dealer/{dealer_id}"
+        reviews = get_request(endpoint)
+
+        if not isinstance(reviews, list):
+            reviews = []
+
+        for review_detail in reviews:
+            sentiment = analyze_review_sentiments(review_detail.get('review', ''))
+            review_detail['sentiment'] = sentiment.get('sentiment', 'neutral')
+
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
